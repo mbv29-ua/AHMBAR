988624450bb21c2eeb36ee5aedab76eff0a632bc
Miguel Beltrá Vidal <mbv29@alu.ua.es>
2025-10-18 16:42:25 +0200

    Add entity setters and an example of use

---
+++ src/entities/enemies/enemies.asm
@@ -0,0 +1,41 @@
+SECTION "Enemies", ROM0
+
+;; Example <- To delete in the final version
+
+init_enemigos_prueba::
+	call man_entity_alloc ;Security allocations, until the code is well structured
+	call man_entity_alloc
+	call man_entity_alloc
+
+	;; Example of initializing an enemy (valid for an entity)
+	call man_entity_alloc ; Returns l=entity index
+	ld b, $78 ; Y coordinate
+	ld c, $14  ; X coordinate
+	ld d, $05 ; tile
+	ld e, 0   ; tile properties
+	call set_entity_sprite
+	ld b, -1 ; vy 
+	ld c,  0 ; vx
+	call set_entity_physics
+
+	call man_entity_alloc ; Returns l=entity index
+	ld b, $55 ; Y coordinate
+	ld c, $56  ; X coordinate
+	ld d, $07 ; tile
+	ld e, %11001010   ; tile properties
+	call set_entity_sprite	
+	ld b,  1 ; vy
+	ld c, -1 ; vx
+	call set_entity_physics
+
+	call man_entity_alloc ; Returns l=entity index
+	ld b, 60 ; Y coordinate
+	ld c, 40  ; X coordinate
+	ld d, $09 ; tile
+	ld e, 0   ; tile properties
+	call set_entity_sprite
+	ld b,  0 ; vy
+	ld c,  1 ; vx
+	call set_entity_physics
+
+	ret
