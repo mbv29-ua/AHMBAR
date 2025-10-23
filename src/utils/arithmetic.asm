3d3fa49101fc27ed052adadb811fcd3edf04c997
Miguel Beltrá Vidal <mbv29@alu.ua.es>
2025-10-23 02:07:57 +0200

    Add common arithmetic routines

--- src/utils/arithmetic.asm
+++ src/utils/arithmetic.asm
@@ -19,3 +19,37 @@ add_bc_de::
 	adc d
 	ld b,a
 	ret
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This routine multiplies the content of the
+;; 16-bit register HL by 32
+;;
+;; INPUT:
+;;		HL: 16-bit integer
+;; OUTPUT:
+;;		HL: 32*HL	
+
+mult_hl_32::
+	add hl, hl      ; * 2
+    add hl, hl      ; * 4
+    add hl, hl      ; * 8
+    add hl, hl      ; * 16
+    add hl, hl      ; * 32
+	ret
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This divides the content of the 8-bit 
+;; register A by 8.
+;;
+;; INPUT:
+;;		A: 8-bit integer
+;; OUTPUT:
+;;		A: A/8	
+
+div_a_8::
+    srl a
+    srl a
+    srl a
+	ret
