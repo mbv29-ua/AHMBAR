ce7bf675f4548a1f50e5c4aaf9627182982be299
Miguel Beltrá Vidal <mbv29@alu.ua.es>
2025-10-24 17:33:38 +0200

    Update scene_manager - add next_scene to scene_conf

--- src/scenes/scene1/scene_1_conf.asm
+++ src/scenes/scene1/scene_1_conf.asm
@@ -8,9 +8,10 @@ scene_1::
 .starting_x: 		DB 16							; SCENE_PLAYER_STARTING_X
 .initial_scroll_y: 	DB 112 							; SCENE_STARTING_SCREEN_SCROLL_Y
 .initial_scroll_x:	DB 0							; SCENE_STARTING_SCREEN_SCROLL_X
-.tileset:			DW tiles 						; SCENE_TILESET ; Se almacen como LOW/HIGH
-.tilset_size:		DW (tiles_end-tiles)			; SCENE_TILESET_SIZE ; Se almacen como LOW/HIGH
-.tileset_offset:	DW 128 * TILE_SIZE				; SCENE_TILESET_OFFSET ; Se almacen como LOW/HIGH
-.tilemap:			DW Level1_Map					; SCENE_TILEMAP ; Se almacen como LOW/HIGH
+.tileset:			DW_BE tiles 					; SCENE_TILESET ; Se almacen como LOW/HIGH
+.tilset_size:		DW_BE (tiles_end-tiles)			; SCENE_TILESET_SIZE ; Se almacen como LOW/HIGH
+.tileset_offset:	DW_BE 128 * TILE_SIZE			; SCENE_TILESET_OFFSET ; Se almacen como LOW/HIGH
+.tilemap:			DW_BE Level1_Map				; SCENE_TILEMAP ; Se almacen como LOW/HIGH
 .goal_x:			DB 0							; SCENE_GOAL_POINT_Y
 .goal_y: 			DB 0							; SCENE_GOAL_POINT_X
+.next_scene:		DW_BE scene_2					; SCENE_NEXT_SCENE
