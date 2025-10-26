893afc06bc21ad86a0153caa5b1975384a149cdc
Miguel Beltrá Vidal <mbv29@alu.ua.es>
2025-10-26 18:34:03 +0100

    Minor changes

--- src/scenes/scene2/scene_2_conf.asm
+++ src/scenes/scene2/scene_2_conf.asm
@@ -8,10 +8,10 @@ scene_2::
 .starting_x: 			DB 		$40						; SCENE_PLAYER_STARTING_X
 .initial_scroll_y: 		DB 		0 						; SCENE_STARTING_SCREEN_SCROLL_Y
 .initial_scroll_x:		DB 		0						; SCENE_STARTING_SCREEN_SCROLL_X
-.tileset:				DW_BE 	tiles					; SCENE_TILESET ; Se almacen como LOW/HIGH
-.tilset_size:			DW_BE 	(tiles_end-tiles)		; SCENE_TILESET_SIZE ; Se almacen como LOW/HIGH
-.tileset_offset:		DW_BE 	128 * TILE_SIZE			; SCENE_TILESET_OFFSET ; Se almacen como LOW/HIGH
-.tilemap:				DW_BE 	Level2_Map				; SCENE_TILEMAP ; Se almacen como LOW/HIGH
+.tileset:				DW_BE 	tiles					; SCENE_TILESET ; Se almacen como HIGH/LOW
+.tilset_size:			DW_BE 	(tiles_end-tiles)		; SCENE_TILESET_SIZE ; Se almacen como HIGH/LOW
+.tileset_offset:		DW_BE 	128 * TILE_SIZE			; SCENE_TILESET_OFFSET ; Se almacen como HIGH/LOW
+.tilemap:				DW_BE 	Level2_Map				; SCENE_TILEMAP ; Se almacen como HIGH/LOW
 .goal_y: 				DB 		0						; SCENE_GOAL_POINT_X
 .goal_x:				DB 		0						; SCENE_GOAL_POINT_Y
 .next_scene:			DW_BE 	scene_1					; SCENE_NEXT_SCENE
