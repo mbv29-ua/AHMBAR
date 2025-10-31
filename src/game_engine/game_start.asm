f45ba88ac51bc52ffb62f449d5fecb7d2e02e43b
nickhernd <ramajahede@gmail.com>
2025-10-31 15:48:05 +0100

    correct tile

--- src/game_engine/game_start.asm
+++ src/game_engine/game_start.asm
@@ -31,8 +31,8 @@ start_game::
 	ld hl, music_game
 	call hUGE_init
 
-	; ld hl, scene_1
-    ld hl, act_1_scene_4
+	ld hl, act_1_scene_1
+    ;ld hl, act_1_scene_4
     ; ld hl, act_2_final_scene
     ; ld hl, act_3_scene_2
     ; ld hl, act_3_scene_1
--- src/scenes/acts/act_1/scene_4/scene_4_conf.asm
+++ src/scenes/acts/act_1/scene_4/scene_4_conf.asm
@@ -9,7 +9,7 @@ act_1_scene_4::
 .initial_scroll_y: 		DB	 	112 					; SCENE_STARTING_SCREEN_SCROLL_Y
 .initial_scroll_x:		DB 		0					; SCENE_STARTING_SCREEN_SCROLL_X
 .tileset:				DW_BE 	tiles 			; SCENE_TILESET ; Se almacen como HIGH/LOW
-.tilset_size:			DW_BE 	(tiles_end-tiles-tiles)	; SCENE_TILESET_SIZE ; Se almacen como HIGH/LOW
+.tilset_size:			DW_BE 	(tiles_end-tiles)	; SCENE_TILESET_SIZE ; Se almacen como HIGH/LOW
 .tileset_offset:		DW_BE 	128 * TILE_SIZE			; SCENE_TILESET_OFFSET ; Se almacen como HIGH/LOW
 .tilemap:				DW_BE 	act_1_scene_4_tilemap				; SCENE_TILEMAP ; Se almacen como HIGH/LOW
 .goal_y: 			DB 		0					; SCENE_GOAL_POINT_X
