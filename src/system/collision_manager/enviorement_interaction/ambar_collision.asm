02a1daa65a2ff4032c519f88b4e54bcdd3d6be55
Miguel Beltrá Vidal <mbv29@alu.ua.es>
2026-04-17 02:35:12 +0200

    Update functions to collect Ahmbar

--- src/system/collision_manager/enviorement_interaction/ambar_collision.asm
+++ src/system/collision_manager/enviorement_interaction/ambar_collision.asm
@@ -31,13 +31,43 @@ check_ambar_collisions::
 ;;; Destroys: A, BC, HL
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;; THIS MUST BE REWRITTEN TO ALLOW MOR FLEXIBILITY
+
 check_ahmbar_tile_collision::
-    ; Is an ahmbar, collect it
+    .loop:
+    .left:
+        call get_current_tile_address_left_point
+        ld a, [hl] ; load tile
+        call is_tile_collectible
+        jp z, .process_collectible
+
+    .right:
+        call get_current_tile_address_right_point
+        ld a, [hl] ; load tile
+        call is_tile_collectible
+        ret nz  ; Not a door, return
+
+    .process_collectible:
+        ld a, [hl] ; load tile
+        ld [hl], EMPTY_TILE
+        call is_tile_ahmbar
+        jp z, .is_ahmbar
+
+        ; Is an ahmbar, collect it
+        .is_ahmbar:
+            call increment_score_and_display
+            ret
+        .another_collectible:
+    jp .loop
+
+
+get_current_tile_address_left_point::
     ld a, [Player.wPlayerY]
     add 4
     ld b, a
     ld a, [Player.wPlayerX]
-    add 4
+    add 1
     ld c, a
     ld a, b
     call convert_y_to_ty
@@ -55,13 +85,29 @@ check_ahmbar_tile_collision::
     add hl, de
     ld de, BG_MAP_START
     add hl, de
-
-    ld a, [hl] ; load tile
-    call is_tile_ahmbar
-    ret nz  ; Not a door, return
-
-    ld [hl], EMPTY_TILE
-    call increment_score_and_display
     ret
 
+get_current_tile_address_right_point::
+    ld a, [Player.wPlayerY]
+    add 4
+    ld b, a
+    ld a, [Player.wPlayerX]
+    add 6
+    ld c, a
+    ld a, b
+    call convert_y_to_ty
+    ld b, a
+    ld a, c
+    call convert_x_to_tx
+    ld c, a
 
+    ld h, 0 ; HL = Tile Y
+    ld l, b
+    call mult_hl_32
+    ; Add Tile X
+    ld d, 0
+    ld e, c ; DE = Tile X 
+    add hl, de
+    ld de, BG_MAP_START
+    add hl, de
+    ret
