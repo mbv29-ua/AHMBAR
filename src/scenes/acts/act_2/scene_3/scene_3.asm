710b006cce55081129bb21e3aef2b645d06ad4e6
Miguel Beltrá Vidal <mbv29@alu.ua.es>
2026-04-25 18:58:00 +0200

    Improve enemy spawn Act 2 scene 3

--- src/scenes/acts/act_2/scene_3/scene_3.asm
+++ src/scenes/acts/act_2/scene_3/scene_3.asm
@@ -1,5 +1,13 @@
 INCLUDE "constants.inc"
+INCLUDE "scenes/acts/act_2/scene_3/act_2_scene_3_constants.inc"
 INCLUDE "entities/entities.inc"
+
+MACRO SPAWN_ENEMY_AT
+    ld b, FMOD(\2 * 8 + 16 + (256 - A2_L3_INITIAL_SCROLL_Y), 256)
+    ld c, FMOD(\1 * 8 +  8 + (256 - A2_L3_INITIAL_SCROLL_X), 256)
+	ld hl, \3
+	call enemy_spawn
+ENDM
 
 SECTION "Act 2 Scene 3", ROM0
 
@@ -14,26 +22,22 @@ act_2_scene_3_intro_dialog::
 
 act_2_scene_3_enemy_spawner::
 
-	ld  b, $90
-	ld  c, $60
-	ld hl, basic_fly
-	call enemy_spawn
+	SPAWN_ENEMY_AT $02, $16, basic_fly
+	SPAWN_ENEMY_AT $0B, $1C, basic_fly
+	SPAWN_ENEMY_AT $0C, $10, basic_fly
+	SPAWN_ENEMY_AT $19, $1B, basic_fly
+	SPAWN_ENEMY_AT $12, $06, basic_fly
 
-	ld  b, $38
-	ld  c, $68
-	ld hl, basic_fly
-	call enemy_spawn
+	SPAWN_ENEMY_AT $18, $09, jumping_frog
+	SPAWN_ENEMY_AT $1B, $0D, jumping_frog
+	SPAWN_ENEMY_AT $18, $11, jumping_frog
+	SPAWN_ENEMY_AT $1B, $15, jumping_frog
 
-	ld  b, $58
-	ld  c, $14
-	ld hl, basic_fly
-
-	call enemy_spawn
 	ret
 
 INCLUDE "system/ambar_macros.inc"
 
 init_ambars_level3::
-    SPAWN_AMBAR_AT_TILE 7, 11
-    SPAWN_AMBAR_AT_TILE 25, 12
+    ;SPAWN_AMBAR_AT_TILE 7, 11
+    ;SPAWN_AMBAR_AT_TILE 25, 12
 	ret
